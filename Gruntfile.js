0153958ae1d9a159888cab3361109617958bbfa9
Mariia Ponomarets <maria.ponomarets@ukr.net>
2016-08-13 22:59:41 +0300

    firs version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,83 @@
-module.exports = function(grunt) {
-    grunt.initConfig({
-        sass: {
-            dist: {
-                files: {
-                    './dist/public/css/main.css': './src/**/main.scss'
-                }
-            }
-        },
-        clean: {
-            all: {
-                src: ['./dist']
-            },
-            css: {
-                src: ['./dist/public/css']
-            },
-            scripts: {
-                src: ['./dist/public/js']
-            },
-            html: {
-                src: ['./dist/public/**/*.html']
-            }
-        },
-        copy: {
-            main: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: './src',
-                        src: ['**/*.html'],
-                        dest: './dist/public'
-                    },
-                    {
-                        expand: true,
-                        cwd: './src',
-                        src: ['**/img/**/*.*'],
-                        dest: './dist/public/'
-                    },
-                    {
-                        expand: true,
-                        cwd: './src',
-                        src: ['**/libs/*.*'],
-                        dest: './dist/public/'
-                    }
-                    
-                ]
-            }
-        },
-        browserify: {
-            dist: {
-                files: {'./dist/public/js/main.js': './src/**/main.js'}
-            }
-        },
-        watch: {
-            sass: {
-                files: ['./src/**/*.scss'],
-                tasks: ['clean:css', 'sass']
-            },
-            scripts: {
-                files: ['./src/js/*.js'],
-                tasks: ['clean:scripts', 'browserify']
-            },
-            html: {
-                files: ['./src/**/*.html'],
-                tasks: ['clean:html', 'copy']
-            }
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          './dist/public/css/main.css': './src/**/main.scss'
         }
-    });
+      }
+    },
+    clean: {
+      all: {
+        src: ['./dist']
+      },
+      css: {
+        src: ['./dist/public/css']
+      },
+      scripts: {
+        src: ['./dist/public/js']
+      },
+      html: {
+        src: ['./dist/public/**/*.html']
+      }
+    },
+    copy: {
+      main: {
+        files: [
+            {
+              expand: true,
+              cwd: './src',
+              src: ['**/*.html'],
+              dest: './dist/public'
+            },
+            {
+              expand: true,
+              cwd: './src',
+              src: ['**/img/**/*.*'],
+              dest: './dist/public/'
+            },
+            {
+              expand: true,
+              cwd: './src',
+              src: ['**/libs/*.*'],
+              dest: './dist/public/'
+            }, {
+              expand: true,
+              cwd: './src/video',
+              src: ['*.*'],
+              dest: './dist/public/video'
+            }
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+        ]
+      }
+    },
+    browserify: {
+      dist: {
+        files: { './dist/public/js/main.js': './src/**/main.js' }
+      }
+    },
+    watch: {
+      sass: {
+        files: ['./src/**/*.scss'],
+        tasks: ['clean:css', 'sass']
+      },
+      scripts: {
+        files: ['./src/js/*.js'],
+        tasks: ['clean:scripts', 'browserify']
+      },
+      html: {
+        files: ['./src/**/*.html'],
+        tasks: ['clean:html', 'copy']
+      }
+    }
+  });
 
-    // Default task(s).
-    grunt.registerTask('default', ['clean:all', 'sass', 'copy', 'browserify']);
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['clean:all', 'sass', 'copy', 'browserify']);
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,41 @@
-﻿
-require('./calendar.js')();
+﻿window.onload = function () {
+  //checkbox and radio
+  var arrCheckBox = document.querySelectorAll('.checkbox');
 
-window.onload = function () {
+  for (var i = 0; i < arrCheckBox.length; i++) {
+    arrCheckBox[i].addEventListener('click', function () {
+      if (this.classList.contains('selected') === true) {
+        this.classList.remove('selected');
+      }
+      else {
+        this.classList.add('selected');
+      }
+    });
+  }
+  var arrRadio = document.querySelectorAll('.radio');
+
+  for (var i = 0; i < arrRadio.length; i++) {
+    arrRadio[i].addEventListener('click', function () {
+      for (var k = 0; k < arrRadio.length; k++) {
+        if (arrRadio[k].classList.contains('selected-radio') === true) {
+          arrRadio[k].classList.remove('selected-radio');
+        }
+      }
+      this.classList.add('selected-radio');
+
+    });
+  }
+
   //login
-  document.getElementById('log-in').addEventListener('click', function () {
+  document.getElementById('log-in').addEventListener('click', function (event) {
+    event.stopImmediatePropagation();
+    event.preventDefault;
     var loggin = document.getElementById('login');
     var pas = document.getElementById('password');
     var error = document.getElementsByClassName('error-container')[0];
     if (loggin.value != '' && pas.value != '') {
       console.log('data send');
-      loggin.value = '';
+      loggin.value = 'John D|';
       pas.value = '';
       if (error.classList.contains('hide-elem') === false) {
         error.classList.add('hide-elem')
@@ -21,7 +47,7 @@ window.onload = function () {
       }
     }
   });
-  
+
   //close dropdown on page
   var buttonClosDrpd = document.getElementById('arrow-open-close');
   var closeDrpd = document.getElementById('close-dropdown');
@@ -45,11 +71,11 @@ window.onload = function () {
 
     }
   });
-  
+
   for (var i = 0; i < childLiCloseDrpd.length; i++) {
     childLiCloseDrpd[i].addEventListener('click', function () {
       console.log(this);
-      if (closeDrpd.classList.contains('hide-elem')===false) {
+      if (closeDrpd.classList.contains('hide-elem') === false) {
         closeDrpd.classList.add('hide-elem');
         buttonClosDrpd.innerHTML = 'arrow_drop_up';
         document.getElementsByClassName('dropdown-container-close')[0].style.height = '62px';
@@ -68,7 +94,7 @@ window.onload = function () {
   var childLiCloseDrpd2 = document.getElementById("close-dropdown2").getElementsByTagName("li")
   buttonClosDrpd2.addEventListener('click', function () {
     if (buttonClosDrpd2.innerHTML === 'arrow_drop_down') {
-     
+
       closeDrpd2.classList.add('hide-elem')
       buttonClosDrpd2.innerHTML = 'arrow_drop_up';
       document.getElementsByClassName('dropdown-container-close')[1].classList.remove('dropdown-open');
@@ -111,5 +137,120 @@ window.onload = function () {
       }
     });
   }
- 
+
+  setInterval(function () {
+    //slider1
+    var buble = document.getElementById('range-val');
+    var val1 = document.getElementById('slider1').value;
+    var tmp1 = 0;
+
+    if (val1 == 0) {
+      tmp1 = -1;
+    }
+    else {
+      tmp1 = val1 * 0.92
+
+    }
+    var margin = tmp1 + '%;';
+    buble.style = 'margin-left:' + margin;
+    buble.innerHTML = val1;
+
+    //slider3
+    var val = document.getElementById('slider3').value + '%';
+    var tmp = 'linear-gradient(to right, #FFA940 ' + val + ', rgba(255, 255, 255, 0) 0%)';
+    document.getElementById('slider3').style.backgroundImage = tmp;
+    //slider2
+    var sl1 = document.getElementById('slider2');
+    var sl2 = document.getElementById('slider2A');
+    var startPos = (sl1.value / 100) * 470;
+    var endPos = (sl2.value / 100) * 470;
+    var widthProggress = endPos - startPos + 'px';
+    sl1.style.backgroundSize = widthProggress;
+    var startInPix = startPos + 'px';
+    sl1.style.backgroundPosition = startInPix;
+  }, 10);
+
+  //video;
+  var player = document.getElementsByTagName('video')[0];
+  var play = document.getElementById('play');
+  var stop = document.getElementById('stop');
+  var progress = document.getElementById('progress-bar');
+  var fullscreen = document.getElementById('full-screen');
+  var valum = document.getElementById('valum');
+  var valOff = document.getElementById('volum-off');
+  var curr = player.currentTime;
+  var dur = player.duration;
+  progress.max = dur;
+  var time;
+  //play-stop
+  play.addEventListener('click', playVideo);
+  function playVideo() {
+    player.play();
+    stop.classList.remove('hide-elem');
+    play.classList.add('hide-elem');
+    time = setInterval(startProgress, 1000);
+  };
+  stop.addEventListener('click', stopVideo);
+  function stopVideo() {
+    player.pause();
+    play.classList.remove('hide-elem');
+    stop.classList.add('hide-elem');
+    clearInterval(time);
+  }
+
+  //fullscreen
+
+  fullscreen.addEventListener('click', function () {
+    if (player.requestFullscreen) {
+      player.requestFullscreen();
+    } else if (player.mozRequestFullScreen) {
+      player.mozRequestFullScreen();
+    } else if (player.webkitRequestFullscreen) {
+      player.webkitRequestFullscreen();
+    }
+  });
+
+  //muted
+  valum.addEventListener('click', volumchange);
+  valOff.addEventListener('click', volumchange);
+  function volumchange() {
+    if (player.volume > 0) {
+      player.volume = 0;
+      valOff.classList.remove('hide-elem');
+      valum.classList.add('hide-elem');
+    } else {
+      player.volume = 0.5;
+      valOff.classList.add('hide-elem');
+      valum.classList.remove('hide-elem');
+    }
+  }
+  //progress
+  progress.addEventListener('change', progressVideo);
+  function progressVideo() {
+    if (player.paused === true) {
+      player.currentTime = progress.value;
+      updateProgress();
+
+    }
+    else {
+      stopVideo();
+      player.currentTime = progress.value;
+      updateProgress();
+
+      playVideo();
+
+    }
+  }
+  function startProgress() {
+    progress.value = player.currentTime;
+    updateProgress();
+  }
+  // setInterval(updateProgress,10)
+  function updateProgress() {
+    var tmp = progress.value / progress.max;
+    var perc = 100 * tmp + '%';
+    var line = 'linear-gradient(to right, #ff7f68 ' + perc + ', rgba(255, 255, 255, 0) 0%)';
+    document.getElementById('progress-bar').style.backgroundImage = line;
+  }
+
 }
